19149d85adbd516a1782ba917c6f726fe963d0b4
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-12-02 15:13:51 +0200

    Final DatabaseFirst project

--- DatabaseFirst/Program.cs
+++ DatabaseFirst/Program.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseFirst_Initial.Services;
+﻿using DatabaseFirst_Initial.Models;
+using DatabaseFirst_Initial.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,13 @@ namespace DatabaseFirst
     {
         static void Main(string[] args)
         {
+
+            IDatabaseService databaseService = new DatabaseService(
+                    new Database("EShop", "dbo.ProductCategories", "SELECT * FROM dbo.ProductCategories"),
+                    new Server("ra1.anystream.eu", "9888"),
+                    new User("sa", "SuperSecretPassw0rd"));
+            databaseService.CreateData(new ProductCategory("Awesome PC", "The Awesome Awesomness of PCs"));
             
-            DatabaseService databaseService = new DatabaseService();
         }
     }
 }
