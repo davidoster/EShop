df31582a9191cd0e1d3ac9789ef2999902b93165
David Oster aka George Pasparakis <paspa@hotmail.com>
2022-12-02 12:40:49 +0200

    1. Created ProductCategory class 2. CRUD more parametrized

---
+++ DatabaseFirst_Initial/Models/ProductCategory.cs
@@ -0,0 +1,24 @@
+﻿using System.CodeDom;
+
+namespace DatabaseFirst_Initial.Models
+{
+    internal class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ProductCategory(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+
+        public ProductCategory(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
--- DatabaseFirst_Initial/Program.cs
+++ DatabaseFirst_Initial/Program.cs
@@ -19,22 +19,26 @@ namespace DatabaseFirst_Initial
                     new User("sa", "SuperSecretPassw0rd")
                 );
             Console.WriteLine(databaseService.ConnectionString);
+            List<ProductCategory> listOfProductCategories = ReadData(databaseService);
+            Console.WriteLine($"Rows: {listOfProductCategories.Count}");
+            //ReadData(databaseService, 190);
 
             // CreateData
-            int id = CreateData(databaseService);
-            Console.WriteLine($"Created row with Id = {id}");
+            //int id = CreateData(databaseService, new ProductCategory("Personal Computer", "The Awesome Personal Computer"));
+            //Console.WriteLine($"Created row with Id = {id}");
 
             // ReadData
-            ReadData(databaseService);
+            //ReadData(databaseService);
 
             // UpdateData
-            UpdateData(databaseService, 178, "Billy", "Billakos");
+            // ReadData(string title, string description) <--- Id = 178
+            //UpdateData(databaseService, new ProductCategory(187, "Personal Computer", "The Personal Computer"));
 
             // DeleteData
-            DeleteData(databaseService, 185);
+            //DeleteData(databaseService, 185);
         }
 
-        private static int CreateData(DatabaseService databaseService)
+        private static int CreateData(DatabaseService databaseService, ProductCategory productCategory)
         {
             int producedId = -1;
             using (SqlConnection sqlConnection = new SqlConnection(databaseService.ConnectionString))
@@ -44,8 +48,8 @@ namespace DatabaseFirst_Initial
                     $"VALUES(@title1, @description1)"; //,(@title2, @description2)";
                 using (SqlCommand command = new SqlCommand(sqlQuery, sqlConnection))
                 {
-                    command.Parameters.AddWithValue("@title1", "Jake");
-                    command.Parameters.AddWithValue("@description1", "United States");
+                    command.Parameters.AddWithValue("@title1", productCategory.Title);
+                    command.Parameters.AddWithValue("@description1", productCategory.Description);
                     //command.Parameters.AddWithValue("@title2", "Jacob");
                     //command.Parameters.AddWithValue("@description2", "UAE");
                     int rowsAffected = command.ExecuteNonQuery();
@@ -68,8 +72,9 @@ namespace DatabaseFirst_Initial
             return producedId;
         }
 
-        private static void ReadData(DatabaseService databaseService)
+        private static List<ProductCategory> ReadData(DatabaseService databaseService, int? id = null)
         {
+            List<ProductCategory> productCategories = new List<ProductCategory>();
             using (SqlConnection connection = // using with resources takes care of closing the connection
                             new SqlConnection(databaseService.ConnectionString)) // connect (δημιουργώ ένα connection)
             {
@@ -85,9 +90,19 @@ namespace DatabaseFirst_Initial
                     connection.Open(); // do the actual connection
                     Console.WriteLine("Yeeeessss Houston we are connected!!!");
                     SqlDataReader reader = command.ExecuteReader(); // Execute
+                    
+                    
                     while (reader.Read()) // για όσο μπορώ να διαβάσω (υπάρχουν εγγραφές) διάβαζε)
                     {
-                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                        var productCategories2 = reader.OfType<ProductCategory>();
+                        Console.WriteLine(productCategories2.FirstOrDefault().Id);
+                        
+                        int someId = reader.GetFieldValue<int>(0); // Id (int)
+                        string someTitle = reader.GetFieldValue<string>(1);
+                        string someDescription = reader.GetFieldValue<string>(2);
+                        Console.WriteLine($"{someId}\t{someTitle}{someDescription}");
+                        productCategories.Add(new ProductCategory(someId, someTitle, someDescription));
+                        //Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
                     }
                     reader.Close();
                 }
@@ -98,35 +113,36 @@ namespace DatabaseFirst_Initial
                 }
                 Console.ReadLine();
                 connection.Close(); // is not needed because of using
+                return productCategories;
             }
         }
 
-        private static void UpdateData(DatabaseService databaseService, int id, string title = null, string description = null)
+        private static void UpdateData(DatabaseService databaseService, ProductCategory productCategory)
         {
-            if (title != null || description != null)
+            if (productCategory.Title != null || productCategory.Description != null)
             {
                 StringBuilder sb = new StringBuilder();
                 sb.Append($"UPDATE {databaseService.Database.Table}");
                 sb.Append(" SET ");
-                if (title != null)
+                if (productCategory.Title != null)
                 {
                     sb.Append("Title");
                     sb.Append("='");
-                    sb.Append(title);
+                    sb.Append(productCategory.Title);
                     sb.Append("'");
                 }
-                if (title != null && description != null)
+                if (productCategory.Title != null && productCategory.Description != null)
                 {
                     sb.Append(",");
                 }
-                if (description != null)
+                if (productCategory.Description != null)
                 {
                     sb.Append("Description");
                     sb.Append("='");
-                    sb.Append(description);
+                    sb.Append(productCategory.Description);
                     sb.Append("'");
                 }
-                sb.Append($" WHERE Id  = {id}");
+                sb.Append($" WHERE Id  = {productCategory.Id}");
                 using (SqlConnection sqlConnection = new SqlConnection(databaseService.ConnectionString))
                 {
                     sqlConnection.Open();
